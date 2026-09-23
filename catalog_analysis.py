ab6302b14864b5363ea9e0ae963ead3ae2485d3d
Ekaterina Shchigol <83863040+ekaterina-shchigol@users.noreply.github.com>
2026-09-23 16:39:37 +0300

    Завершён этап 3: циклы

--- catalog_analysis.py
+++ catalog_analysis.py
@@ -126,3 +126,27 @@ def decade_label(year):
             return "недавние"
         case _:
             return "старые"
+
+
+# Этап 3. Циклы
+
+for movie in movies:
+    if "comedy" in movie["genres"]:
+        continue
+    print(movie["title"])
+
+i = 0
+while i < len(movies):
+    if movies[i]["rating"] > 9.0:
+        print(movies[i]["title"])
+        break
+    i += 1
+else:
+    print("Шедевров не найдено")
+
+def count_long_movies(movies, threshold=120):
+    count = 0
+    for movie in movies:
+        if movie["duration_min"] > threshold:
+            count += 1
+    return count
